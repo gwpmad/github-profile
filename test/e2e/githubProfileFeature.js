05081b26ffc3e5528d450f859df530b4ca1e0489
alaanzr <alaanzr@gmail.com>
2015-12-03 13:07:51 +0000

    Testing response returned from our search

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -13,9 +13,16 @@ describe('Github Profile finder', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
 
-  it('finds profiles', function() {
-    searchBox.sendKeys('spike01');
+  it('finds the last Spike', function() {
+    searchBox.sendKeys('spike');
     searchButton.click();
-    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+      profiles.last().getText().then(function(response) {
+        // http://stackoverflow.com/questions/28811661/protractor-return-an-object-but-expected-value-of-element-gettext
+        // getText() returns a promise, therefore we cannot treat the response as a string. In order to
+        // manipulate the data as a string, we need to resolve the promise by suffix-ing gettext() with .then
+        // The response is passed as an argument to .then, and can subsequently be manipulated and tested.
+      expect(response.toLowerCase()).toContain('spike');
+    });
   });
 });
